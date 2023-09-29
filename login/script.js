0e78e177dfb2a557c8ffe537826fea33f31a6a80
Wonnnyo Hamester <wonnyohamester@outlook.com>
2023-09-28 21:21:26 -0700

    feat: Login with email and password

--- login/script.js
+++ login/script.js
@@ -1,13 +1,31 @@
-import { api_url, google_id } from "../constants.js";
+import { api_url, google_id } from "../js/constants.js";
+import { getUserSession, setUserSession } from "../js/storage.js";
 
 /**
  * Google Auth Setup
  */
 window.onload = function () {
+  const user = getUserSession();
+
+  if (!!user?.id) {
+    window.location.replace("/");
+  }
+
   google.accounts.id.initialize({
     client_id: google_id,
-    callback: async (response) => {
-      console.log({ response });
+    callback: async (googleResponse) => {
+      try {
+        const response = await fetch(`${api_url}/user?provider=google`, {
+          method: "POST",
+          body: JSON.stringify(googleResponse),
+        });
+
+        const { data } = await response.json();
+        setUserSession(data);
+        window.location.replace("/");
+      } catch (error) {
+        console.debug(error);
+      }
     },
     use_fedcm_for_prompt: true,
   });
@@ -31,10 +49,13 @@ document
 
     const response = await fetch(`${api_url}/auth`, {
       method: "POST",
-      body: JSON.stringify({ email, password }),
-      mode: "no-cors",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
     });
 
-    const data = await response.json();
-    console.log({ data });
+    const { data } = await response.json();
+    setUserSession(data);
+    window.location.replace("/");
   });
