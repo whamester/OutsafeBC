e07b9b24191a1c2a7acb7967bafa658ae108e74a
Wonnnyo Hamester <wonnyohamester@outlook.com>
2023-09-29 08:43:56 -0700

    feat: implementing sign out

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+import { api_url } from "./constants.js";
 import { clearUserSession, getUserSession } from "./storage.js";
 
 window.onload = function () {
@@ -12,7 +13,19 @@ window.onload = function () {
   }
 };
 
-document.getElementById("logout-btn").addEventListener("click", () => {
+document.getElementById("logout-btn").addEventListener("click", async () => {
+  const user = getUserSession();
+
+  if (!!user?.auth?.currentUser) {
+    try {
+      await fetch(`${api_url}/signout`, {
+        method: "POST",
+        body: JSON.stringify(user.auth),
+      });
+    } catch (error) {
+      console.debug(error);
+    }
+  }
   clearUserSession();
   window.location.reload();
 });
