fb7ef4c3bf35cd1ddf4794469cb98ac8a8a9e7c3
Reinhardt Botha <botha.wr@gmail.com>
2023-10-04 23:09:26 -0700

    optimized modal toggle JS, drop area for image upload created

--- settings/script.js
+++ settings/script.js
@@ -1,66 +1,84 @@
 //Show user information
-import { getUserSession } from "../js/storage.js";
+import { getUserSession, setUserSession } from "../js/storage.js";
 
 const user = getUserSession();
 
 function showUserInfo() {
   if (user) {
-  document.getElementById("name").setAttribute("value", user?.name);
-  document.getElementById("email").setAttribute("value", user?.email);
+    document.getElementById("name").setAttribute("value", user?.name);
+    document.getElementById("email").setAttribute("value", user?.email);
   } else {
-    myProfile.style.display = "none"
-    deleteAccount.style.display = "none"
+    myProfile.style.display = "none";
+    deleteAccount.style.display = "none";
   }
 }
 
 showUserInfo();
 
 // Change user information
-let userName = user?.name
-let userEmail = user?.email
+let userName = user?.name;
+let userEmail = user?.email;
 
 saveProfileInfoBtn.addEventListener("click", () => {
-  userName = document.getElementById("name").value
-  user.name = userName
-  userEmail = document.getElementById("email").value
-  user.email = userEmail
-  localStorage.setItem("user", JSON.stringify(user));
-  showProfilePic()
-})
+  userName = document.getElementById("name").value;
+  user.name = userName;
+  userEmail = document.getElementById("email").value;
+  user.email = userEmail;
+  setUserSession(user);
+  showProfilePic();
+});
 
 // Change password
-changePwBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  changePwPopup.style.display = "block";
-});
-
-changePwSaveBtn.addEventListener("click", () => {
-  changePwPopup.style.display = "none";
-});
-
-// Profile photo
-function showProfilePic (){
-  profilePhoto.setAttribute("src", user?.photo)
+function togglePwModal() {
+  const pwModal = changePwPopup.style;
+  pwModal.display = pwModal.display === "block" ? "none" : "block";
 }
 
-showProfilePic()
+changePwBtn.addEventListener("click", togglePwModal);
 
-let userPhoto = user?.photo
+changePwSaveBtn.addEventListener("click", togglePwModal);
 
-changeProfilePhotoBtn.onchange = () => {
-  const photoURL = URL.createObjectURL(changeProfilePhotoBtn.files[0]);
-  userPhoto = photoURL;
-  user.photo = userPhoto;
-  localStorage.setItem("user", JSON.stringify(user));
-  showProfilePic()
-};
+// Profile photo
 
+function showProfilePic() {
+  profilePhoto.setAttribute("src", user?.photo);
+}
+
+showProfilePic();
+
+const dropArea = document.getElementById("fileDropWrapper");
+
+dropArea.addEventListener("dragover", (e) => {
+  e.preventDefault();
+});
+
+dropArea.addEventListener("drop", (e) => {
+  e.preventDefault();
+
+  const picture = e.dataTransfer.files[0];
+  const fileType = picture.type;
+
+  if (
+    fileType == "image/png" ||
+    fileType == "image/jpg" ||
+    fileType == "image/jpeg"
+  ) {
+    let userPhoto = user?.photo;
+
+    const photoURL = URL.createObjectURL(picture);
+    userPhoto = photoURL;
+    user.photo = userPhoto;
+    setUserSession(user);
+    showProfilePic();
+  }
+});
 
 // Delete profile
-deleteAccountBtn.addEventListener("click", () => {
-  deleteAccountConfirm.style.display = "block";
-});
+function toggleDelModal() {
+  const delModal = deleteAccountConfirm.style;
+  delModal.display = delModal.display === "block" ? "none" : "block";
+}
 
-deleteAccountNoBtn.addEventListener("click", () => {
-  deleteAccountConfirm.style.display = "none";
-});
+deleteAccountBtn.addEventListener("click", toggleDelModal);
+
+deleteAccountNoBtn.addEventListener("click", toggleDelModal);
