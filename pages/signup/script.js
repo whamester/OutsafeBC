165fa100704d933c30e1bf6cd620a9b47d276f95
Wonnnyo Hamester <wonnyohamester@outlook.com>
2023-10-12 09:05:24 -0700

    chore: adding prettierrc config

--- pages/signup/script.js
+++ pages/signup/script.js
@@ -1,73 +1,71 @@
-import { API_URL, GOOGLE_ID } from "../../constants.js";
-import {
-  getUserSession,
-  setUserSession,
-} from "../../assets/helpers/storage.js";
+import { API_URL, GOOGLE_ID } from '../../constants.js'
+import { getUserSession, setUserSession } from '../../assets/helpers/storage.js'
 
 /**
  * Google Auth Setup
  */
+
 window.onload = function () {
-  const user = getUserSession();
+	const user = getUserSession()
 
-  if (!!user?.id) {
-    window.location.replace("/");
-  }
+	if (!!user?.id) {
+		window.location.replace('/')
+	}
 
-  google.accounts.id.initialize({
-    client_id: GOOGLE_ID,
-    callback: async (googleResponse) => {
-      try {
-        const response = await fetch(`${API_URL}/user?provider=google`, {
-          method: "POST",
-          body: JSON.stringify(googleResponse),
-        });
+	google.accounts.id.initialize({
+		client_id: GOOGLE_ID,
+		callback: async (googleResponse) => {
+			try {
+				const response = await fetch(`${API_URL}/user?provider=google`, {
+					method: 'POST',
+					body: JSON.stringify(googleResponse),
+				})
 
-        const { data } = await response.json();
+				const { data } = await response.json()
 
-        if (data?.id) {
-          setUserSession(data);
-          window.location.replace("/");
-        }
-      } catch (error) {
-        console.debug({ error });
-      }
-    },
-    use_fedcm_for_prompt: true,
-  });
-  google.accounts.id.renderButton(document.getElementById("google-button"), {
-    theme: "outline",
-    size: "large",
-  });
-  google.accounts.id.prompt();
-};
+				if (data?.id) {
+					setUserSession(data)
+					window.location.replace('/')
+				}
+			} catch (error) {
+				console.debug({ error })
+			}
+		},
+		use_fedcm_for_prompt: true,
+	})
+	google.accounts.id.renderButton(document.getElementById('google-button'), {
+		theme: 'outline',
+		size: 'large',
+	})
+	google.accounts.id.prompt()
+}
 
 /**
  * Create Account Submit
  */
 document
-  .getElementById("signup-form")
-  .addEventListener("submit", async (event) => {
-    event.preventDefault();
+	.getElementById('signup-form')
+	.addEventListener('submit', async (event) => {
+		event.preventDefault()
 
-    try {
-      const email = document.getElementById("email-input").value;
-      const password = document.getElementById("password-input").value;
+		try {
+			const email = document.getElementById('email-input').value
+			const password = document.getElementById('password-input').value
 
-      const response = await fetch(`${API_URL}/user?provider=password`, {
-        method: "POST",
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      });
+			const response = await fetch(`${API_URL}/user?provider=password`, {
+				method: 'POST',
+				body: JSON.stringify({
+					email,
+					password,
+				}),
+			})
 
-      const { data } = await response.json();
-      if (data?.id) {
-        setUserSession(data);
-        window.location.replace("/");
-      }
-    } catch (error) {
-      console.debug(error);
-    }
-  });
+			const { data } = await response.json()
+			if (data?.id) {
+				setUserSession(data)
+				window.location.replace('/')
+			}
+		} catch (error) {
+			console.debug(error)
+		}
+	})
