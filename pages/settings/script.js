84756de030351d905b7433564c5f0de9b06afbec
Reinhardt Botha <botha.wr@gmail.com>
2023-10-12 16:40:05 -0700

    error handling for profile pic, initial API update user info

--- pages/settings/script.js
+++ pages/settings/script.js
@@ -1,84 +1,109 @@
 //Show user information
-import { getUserSession, setUserSession } from "../../assets/helpers/storage.js";
+import { getUserSession, setUserSession } from '../../assets/helpers/storage.js'
+import { API_URL } from '../../constants.js'
 
-const user = getUserSession();
+const user = getUserSession()
 
 function showUserInfo() {
-  if (user) {
-    document.getElementById("name").setAttribute("value", user?.name);
-    document.getElementById("email").setAttribute("value", user?.email);
-  } else {
-    myProfile.style.display = "none";
-    deleteAccount.style.display = "none";
-  }
+	if (user) {
+		document.getElementById('name').setAttribute('value', user?.name)
+		document.getElementById('email').setAttribute('value', user?.email)
+	} else {
+		myProfile.style.display = 'none'
+		deleteAccount.style.display = 'none'
+	}
 }
 
-showUserInfo();
+showUserInfo()
 
 // Change user information
-let userName = user?.name;
-let userEmail = user?.email;
+let userName = user?.name
+let userEmail = user?.email
+let userID = user?.id
 
-saveProfileInfoBtn.addEventListener("click", () => {
-  userName = document.getElementById("name").value;
-  user.name = userName;
-  userEmail = document.getElementById("email").value;
-  user.email = userEmail;
-  setUserSession(user);
-  showProfilePic();
-});
+saveProfileInfoBtn.addEventListener('click', () => {
+	userName = document.getElementById('name').value
+	userEmail = document.getElementById('email').value
+	user.name = userName
+	user.email = userEmail
+	setUserSession(user)
+	showProfilePic()
+	saveUserInfo()
+	console.log(`${API_URL}/user?id=${userID}`)
+})
+
+// Save user information
+async function saveUserInfo() {
+	try {
+		const name = document.getElementById('name').value
+
+		const response = await fetch(`${API_URL}/user?id=${userID}`, {
+			method: 'PUT',
+			body: JSON.stringify({
+				name,
+			}),
+		})
+		const result = await response.json()
+		console.log('success', result)
+	} catch (error) {
+		console.error('error', error)
+	}
+}
 
 // Change password
 function togglePwModal() {
-  const pwModal = changePwPopup.style;
-  pwModal.display = pwModal.display === "block" ? "none" : "block";
+	const pwModal = changePwPopup.style
+	pwModal.display = pwModal.display === 'block' ? 'none' : 'block'
 }
 
-changePwBtn.addEventListener("click", togglePwModal);
+changePwBtn.addEventListener('click', togglePwModal)
 
-changePwSaveBtn.addEventListener("click", togglePwModal);
+changePwSaveBtn.addEventListener('click', togglePwModal)
 
 // Profile photo
-
 function showProfilePic() {
-  profilePhoto.setAttribute("src", user?.photo);
+	if (user && user.photo) {
+		profilePhoto.setAttribute('src', user?.photo)
+	} else {
+		profilePhoto.setAttribute('src', '#')
+	}
 }
 
-showProfilePic();
+showProfilePic()
 
-const dropArea = document.getElementById("fileDropWrapper");
+const dropArea = document.getElementById('fileDropWrapper')
 
-dropArea.addEventListener("dragover", (e) => {
-  e.preventDefault();
-});
+dropArea.addEventListener('dragover', (e) => {
+	e.preventDefault()
+})
 
-dropArea.addEventListener("drop", (e) => {
-  e.preventDefault();
+dropArea.addEventListener('drop', (e) => {
+	e.preventDefault()
 
-  const picture = e.dataTransfer.files[0];
-  const fileType = picture.type;
+	const picture = e.dataTransfer.files[0]
+	const fileType = picture.type
 
-  if (
-    fileType == "image/png" ||
-    fileType == "image/jpg" ||
-    fileType == "image/jpeg"
-  ) {
-    let userPhoto = user?.photo;
+	if (
+		fileType == 'image/png' ||
+		fileType == 'image/jpg' ||
+		fileType == 'image/jpeg'
+	) {
+		let userPhoto = user?.photo
 
-    const photoURL = URL.createObjectURL(picture);
-    userPhoto = photoURL;
-    user.photo = userPhoto;
-    setUserSession(user);
-    showProfilePic();
-  }
-});
+		const photoURL = URL.createObjectURL(picture)
+		userPhoto = photoURL
+		user.photo = userPhoto
+		setUserSession(user)
+		showProfilePic()
+	}
+})
 
 // Delete profile
 function toggleDelModal() {
-  const delModal = deleteAccountConfirm.style;
-  delModal.display = delModal.display === "block" ? "none" : "block";
+	const delModal = deleteAccountConfirm.style
+	delModal.display = delModal.display === 'block' ? 'none' : 'block'
 }
 
-deleteAccountBtn.addEventListener("click", toggleDelModal);
+deleteAccountBtn.addEventListener('click', toggleDelModal)
 
-deleteAccountNoBtn.addEventListener("click", toggleDelModal);
+deleteAccountNoBtn.addEventListener('click', toggleDelModal)
