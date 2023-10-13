84bd1623b9b5368de01d8dfb5853f7003c530459
ALFAROSO <alberth.sotelo@gmail.com>
2023-10-13 11:32:32 -0700

    comments from whamester

--- pages/report-hazard/script.js
+++ pages/report-hazard/script.js
@@ -6,6 +6,7 @@ import Map from '../../assets/models/Map.js'
 const currentReport = new ReportForm()
 let position = Map.DEFAULT_LOCATION
 let map = null
+const API_URL = 'https://enchanting-llama-6664aa.netlify.app/.netlify/functions'
 
 /**
  * Page Init
@@ -70,30 +71,43 @@ const onSelectLocation = (event) => {
  * Step 2: Category List
  */
 
-async function getCategory() {
-	const baseURL =
-		'https://enchanting-llama-6664aa.netlify.app/.netlify/functions/hazard-category'
+const getCategories = async () => {
+	try {
+		let response = await fetch(`${API_URL}/hazard-category`)
+		let { data } = await response.json()
+		const content = document.getElementById('hazard-category')
 
-	let res = await fetch(baseURL)
-	let data = await res.json()
+		for (let i = 0; i < data.length; i++) {
+			const category = data[i]
+			const div = document.createElement('div')
+			const radio = document.createElement('input')
 
-	for (let i = 0; i < data.data.length; i++) {
-		catego = data.data[i].name
-		var catego = document.getElementById('category' + (i + 1))
-		catego.innerHTML = data.data[i].name
+			radio.setAttribute('type', 'radio')
+			radio.setAttribute('name', 'categoryRadioBtn')
+			radio.setAttribute('id', `category-${category.id}-radio`)
+			radio.setAttribute('value', category.id)
+			radio.addEventListener('change', (event) => {
+				window.location.href = '#hazard-type'
+				currentReport.categoryId = event.target.value
+			})
+
+			const label = document.createElement('label')
+
+			label.setAttribute('id', `category-${category.id}-label`)
+			label.setAttribute('for', `category-${category.id}-radio`)
+			label.innerHTML = category.name
+
+			div.appendChild(radio)
+			div.appendChild(label)
+
+			content.appendChild(div)
+		}
+	} catch (error) {
+		console.error(error)
 	}
 }
-getCategory()
 
-document
-	.querySelectorAll('[name="categoryRadioBtn"]')
-	.forEach((categoryElement) => {
-		categoryElement.addEventListener('change', (event) => {
-			window.location.href = '#hazard-type'
-
-			currentReport.categoryId = event.target.value
-		})
-	})
+getCategories()
 
 /**
  * Step 3: Hazard Options List
