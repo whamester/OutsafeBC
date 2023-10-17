a51d1eee8ddf9ee61ebb175f5292e79178221d27
Reinhardt Botha <botha.wr@gmail.com>
2023-10-17 15:20:04 -0700

    initial helper js files

---
+++ assets/helpers/report-card.js
@@ -0,0 +1,9 @@
+import ReportCard from './card-container.js'
+
+class MyReport extends ReportCard {
+	constructor() {
+		super(id, category, hazard, location, date, photos, comment)
+	}
+}
+
+export default MyReport
