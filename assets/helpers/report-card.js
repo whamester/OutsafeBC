650bd4e97061bea721631621560a65f057c4e840
Reinhardt Botha <botha.wr@gmail.com>
2023-10-17 16:59:30 -0700

    fetch recent report data from endpoint and display it using a the MyReport class object

--- assets/helpers/report-card.js
+++ assets/helpers/report-card.js
@@ -1,8 +1,23 @@
 import ReportCard from './card-container.js'
 
 class MyReport extends ReportCard {
-	constructor() {
+	constructor(id, category, hazard, location, date, photos, comment) {
 		super(id, category, hazard, location, date, photos, comment)
+	}
+
+	reportContent() {
+		let div = document.createElement('div')
+		div.setAttribute('id', `reportCard${this.id}`)
+		div.innerHTML = `
+        <h2 class="hazardTitle">${this.hazard}</h2>
+		<p class="hazardLocation">${this.location}</p>
+		<p class="hazardDate">${this.date}</p>
+		<p class="hazardTime">${this.date}</p>
+		<div class="hazardPohotos">${this.photos}</div>
+		<p class="hazardDescTitle">Description</p>
+		<p class="hazardDesc">${this.comment}</p>
+        `
+		return div
 	}
 }
 
--- pages/my-reports/script.js
+++ pages/my-reports/script.js
@@ -1,3 +1,48 @@
-import { API_URL } from "../../constants.js";
-let reportData = []
+import MyReport from '../../assets/helpers/report-card.js'
+import { API_URL } from '../../constants.js'
+import { getUserSession } from '../../assets/helpers/storage.js'
 
+const user = getUserSession()
+let userID = user?.id
+let recentReportArr = []
+let olderReportArr = []
+const recentReports = document.getElementById('recentReports')
+const olderReports = document.getElementById('olderReports')
+const recentBtn = document.getElementById('recentReportsBtn')
+const olderBtn = document.getElementById('olderReportsBtn')
+
+// Get all the recent reports for the logged in user and display them
+
+async function getRecentReports() {
+	try {
+		recentReportArr.splice(0, recentReportArr.length)
+
+		const response = await fetch(
+			`${API_URL}/hazard-report?cursor=20&size=40&user_id=${userID}&type=recent`
+		)
+		const result = await response.json()
+
+		recentReportArr.push(...result.data.results)
+	} catch (error) {
+		console.log('Could not get user reports', error)
+	}
+}
+
+async function displayRecentReports() {
+	await getRecentReports()
+
+	for (const index of recentReportArr) {
+		let hazardReport = new MyReport(
+			index.id,
+			index.hazardCategory.name,
+			index.hazard.name,
+			index.location.address,
+			index.created_at,
+			index.images,
+			index.comment
+		)
+		recentReports.appendChild(hazardReport.reportContent())
+	}
+}
+
+displayRecentReports()
