ee9c79660a731625b33fbc535050c8314b1d2015
Wonnnyo Hamester <wonnyohamester@outlook.com>
2023-10-18 16:11:11 -0700

    feat: jawg-terrain map

--- assets/models/Map.js
+++ assets/models/Map.js
@@ -19,19 +19,8 @@ class Map {
 			Map.CURRENT_ZOOM
 		)
 
-		// L.tileLayer(
-		// 	'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}',
-		// 	{
-		// 		// minZoom: 0,
-		// 		// maxZoom: 20,
-		// 		attribution:
-		// 			'&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-		// 		ext: 'png',
-		// 	}
-		// ).addTo(this.map)
-
 		L.tileLayer(
-			`https://{s}.tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token=${JAWG_ACCESS_TOKEN}`,
+			`https://{s}.tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token=${JAWG_ACCESS_TOKEN}`,
 			{
 				attribution:
 					'<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
