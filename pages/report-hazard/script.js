57aa50759090227c7dbe389cf988a5dabbef95d2
ALFAROSO <alberth.sotelo@gmail.com>
2023-10-21 21:35:17 -0700

    testing show and hide final mobile and pc

--- pages/report-hazard/script.js
+++ pages/report-hazard/script.js
@@ -134,17 +134,11 @@ commentInput.addEventListener('change', (event) => {
  */
 
 function checkMobileDevice() {
-
-	let isMobileDevice;
-
 	if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
-	//   isMobileDevice = true;
 	  document.getElementById("desktop").style.display = "none"
 	} else {
-	//   isMobileDevice = false;
 	  document.getElementById("mobile").style.display = "none"
 	}
-	// return isMobileDevice;
   }
 
   checkMobileDevice();
