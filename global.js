cc6a0a8dd51f1f01b5447321bfdb1acb06d5d811
Reinhardt Botha <botha.wr@gmail.com>
2023-10-23 20:57:35 -0700

    removed default body margin of 8px and moved components.html into pages and fixed relative icon paths in global.js

--- global.js
+++ global.js
@@ -18,13 +18,13 @@ const loadIcons = () => {
 			const iconName = classes.find((className) => className.includes('icon-'))
 
 			if (iconName) {
-				element.style.mask = `url(assets/icons/${iconName.replace(
+				element.style.mask = `url(../../assets/icons/${iconName.replace(
 					'icon-',
 					''
 				)}.svg)`
 				element.style[
 					'-webkit-mask-image'
-				] = `url(assets/icons/${iconName.replace('icon-', '')}.svg)`
+				] = `url(../../assets/icons/${iconName.replace('icon-', '')}.svg)`
 				element.style['mask-size'] = 'cover'
 				element.style['-webkit-mask-size'] = 'cover'
 			}
@@ -55,7 +55,7 @@ const errorInputHelper = () => {
 
 		icon.setAttribute('class', 'icon-exclamation-mark')
 		icon.style.mask = `url(assets/icons/exclamation-mark.svg)`
-		icon.style['-webkit-mask-image'] = `url(assets/icons/exclamation-mark.svg)`
+		icon.style['-webkit-mask-image'] = `url(../../assets/icons/exclamation-mark.svg)`
 		icon.style['mask-size'] = 'cover'
 		icon.style['-webkit-mask-size'] = 'cover'
 		element.appendChild(icon)
