d459103d473bf92e8c2ae9ecf4491568af762f6a
Reinhardt Botha <botha.wr@gmail.com>
2023-10-23 22:11:31 -0700

    removed unused classes from report-card.js helper

--- assets/helpers/report-card.js
+++ assets/helpers/report-card.js
@@ -9,13 +9,13 @@ class MyReport extends ReportCard {
 		let div = document.createElement('div')
 		div.setAttribute('id', `reportCard${this.id}`)
 		div.innerHTML = `
-        <h2 class="hazardTitle">${this.hazard}</h2>
-		<p class="hazardLocation">${this.location}</p>
-		<p class="hazardDate">${this.date}</p>
-		<p class="hazardTime">${this.date}</p>
-		<div class="hazardPohotos">${this.photos}</div>
-		<p class="hazardDescTitle">Description</p>
-		<p class="hazardDesc">${this.comment}</p>
+        <h2>${this.hazard}</h2>
+		<p>${this.location}</p>
+		<p>${this.date}</p>
+		<p>${this.date}</p>
+		<div>${this.photos}</div>
+		<p>Description</p>
+		<p>${this.comment}</p>
         `
 		return div
 	}
