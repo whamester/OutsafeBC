d29f75c9f99b6b7ae7ab9967ece13a730522ad78
Wonnnyo Hamester <wonnyohamester@outlook.com>
2023-10-26 15:04:57 -0700

    feat: header WIP

--- assets/components/Navbar.js
+++ assets/components/Navbar.js
@@ -1,9 +1,7 @@
 const Navbar = () => {
 	return `
-  <header class="w-100">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand fs-1 fw-bold mx-4" href="#">Outsafe BC</a>
-      <div class="input-group">
+  <header>
+    <nav>
     </nav>
   </header>
   `
