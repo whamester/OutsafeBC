cb927f82856ffe5ef66664a76317f5b60d9f6728
ALFAROSO <alberth.sotelo@gmail.com>
2023-10-27 00:20:35 -0700

    print pictures on mobile browser 10

--- pages/report-hazard/script.js
+++ pages/report-hazard/script.js
@@ -429,9 +429,10 @@ function renderPhotos() {
 	imagesFirstOutput2.innerHTML = ''
 
 	for (let i = 0; i < arrayPict.length; i++) {
-		imagesFirstOutput2.appendChild(arrayPict[i])
-		imagesFirstOutput3.innerHTML += arrayPict[i]
-		console.log(arrayPict[i])
+		imagesFirstOutput2.innerHTML += `<img src="${arrayPict[i]}" width="150" />`
+		// imagesFirstOutput2.appendChild(arrayPict[i])
+		// imagesFirstOutput3.innerHTML += arrayPict[i]
+		// console.log(arrayPict[i])
 	}
 }
 
