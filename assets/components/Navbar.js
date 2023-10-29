21e7148873faf91faf37d5faad83c843bfa0b1cf
Wonnnyo Hamester <wonnyohamester@outlook.com>
2023-10-29 15:40:45 -0700

    chore: link the avatar in the header with settings page

--- assets/components/Navbar.js
+++ assets/components/Navbar.js
@@ -8,8 +8,8 @@ const Navbar = () => {
 
 	return `
   <header>
+  <div id="header-content">
     <a href="/pages/home"><img src="../../assets/img/header-logo-dark.svg" style="width:8.06rem; height:100%" alt="Outsafe BC logo" /></a>
-
     ${
 			!!showNavigation
 				? ` <nav id="header-nav">
@@ -21,15 +21,16 @@ const Navbar = () => {
             <i class="icon-notification-no"></i>
             <i class="icon-notification-yes hidden"></i>
           </div>
-          <img id="avatar" src="${user.photo}" alt="User logo" />`
+          <a href="/pages/settings"><img id="avatar" src="${user.photo}" alt="User logo" /></a>`
 					: `<a href="/pages/login"><button class="btn btn-secondary">Sign in</button></a>`
 			} 
     </nav>`
 				: ``
 		}
-   
+   </div>
   </header>
   `
 }
-
+// TODO: Temporarily sending the user to the profile by clicking the avatar icon, this will
+// be replaced by the context menu
 export default Navbar
