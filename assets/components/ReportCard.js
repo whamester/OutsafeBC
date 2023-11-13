58385e20494fcd150f5d64a2c220f937d64dd022
Reinhardt Botha <botha.wr@gmail.com>
2023-11-13 12:25:37 -0800

    initial commit

--- assets/components/ReportCard.js
+++ assets/components/ReportCard.js
@@ -13,62 +13,10 @@ class MyReportCard extends ReportCard {
     let divInner = document.createElement('div');
     divInner.setAttribute('class', `report-card__inner`);
 
-    const icon = this.settings?.detail;
-    divInner.innerHTML = `
-		<div class="report-card__heading">
-			<span class="btn__icon report-card__heading__icon" style="background-color: ${
-        icon.iconBackround
-      }">
-				<i class="${
-          icon.icon
-        }" style="width:1.5rem; height:1.5rem; background-color: white"></i>
-			</span>
-        	<p class="text-body-1 semibold">${this.hazard}</p>
-		</div>
-
-		<div class="report-card__top-info">
-			<div class="report-card__details">
-				<i class="icon-location-pin-outline" style="background-color: var(--neutral-400)"></i>
-				<p class="text-body-2 regular report-card__location-text">${this.location}</p>
-			</div>
-
-			<div class="report-card__date_time">
-				<div class="report-card__details">
-					<i class="icon-date" style="background-color: var(--neutral-400)"></i>
-					<p class="text-body-2 regular">${super.getDateFormatted()}</p>
-				</div>
-
-				<div class="report-card__details">
-					<i class="icon-time" style="background-color: var(--neutral-400)"></i>
-					<p class="text-body-2 regular">${super.getTimeFormatted()}</p>
-				</div>
-			</div>
-		</div>
-
-		<div class="report-card__spacer-line"></div>
-				
-		<div id="report-card__image-gallery"></div>
-
-		<div class="report-card__spacer-line"></div>
-
-		<p class="text-body-3 regular">Description</p>
-		<p class="text-body-2 regular">${this.comment}</p>
-
-		<div class="report-card__spacer-line"></div>
-
-		<div class="report-card__my-reports-buttons">
-			${ToggleSwitch(this.id)}
-			<button class="btn btn-tertiary text-body-3 medium" onclick="window.location.href='/pages/report-hazard/index.html?id=${
-        this.id
-      }#review-report'">
-				<i class="icon-edit"></i>
-				Edit Report
-			</button>
-		</div>
-        `;
-    divInner
-      .querySelector('#report-card__image-gallery')
-      .appendChild(super.getGallery());
+    divInner.appendChild(super.getHeading());
+    divInner.appendChild(super.getTopInfo());
+    divInner.appendChild(super.getGallery());
+  
     divOuter.appendChild(divInner);
     return divOuter;
   }
--- assets/helpers/card-container.js
+++ assets/helpers/card-container.js
@@ -20,8 +20,51 @@ class ReportCard {
     return DateFormat.getTime(this.date);
   }
 
+  getHeading() {
+    const icon = this.settings?.detail;
+    const contentHTML = `
+			<span class="btn__icon report-card__heading__icon" style="background-color: ${icon.iconBackround}">
+				<i class="${icon.icon}" style="width:1.5rem; height:1.5rem; background-color: white"></i>
+			</span>
+      <p class="text-body-1 semibold">${this.hazard}</p>
+    `;
+    const div = document.createElement('div');
+    div.setAttribute('class', 'report-card__heading');
+    div.innerHTML = contentHTML;
+    return div;
+  }
+
+  getTopInfo() {
+    const contentHTML = `
+    <div class="report-card__details">
+      <i class="icon-location-pin-outline" style="background-color: var(--neutral-400)"></i>
+      <p class="text-body-2 regular report-card__location-text">${
+        this.location
+      }</p>
+    </div>
+
+    <div class="report-card__date_time">
+      <div class="report-card__details">
+        <i class="icon-date" style="background-color: var(--neutral-400)"></i>
+        <p class="text-body-2 regular">${this.getDateFormatted()}</p>
+      </div>
+
+      <div class="report-card__details">
+        <i class="icon-time" style="background-color: var(--neutral-400)"></i>
+        <p class="text-body-2 regular">${this.getTimeFormatted()}</p>
+      </div>
+    </div>
+  `;
+    const div = document.createElement('div');
+    div.setAttribute('class', 'report-card__top-info');
+    div.innerHTML = contentHTML;
+    return div;
+  }
+
   getGallery() {
     let photos = this.photos;
+    let galleryContainer = document.createElement('div');
+    galleryContainer.setAttribute('id', 'report-card__image-gallery');
     let gallery = document.createElement('div');
     gallery.setAttribute('id', 'report-card__picture-container');
 
@@ -30,7 +73,8 @@ class ReportCard {
       image.src = pic;
       gallery.appendChild(image);
     }
-    return gallery;
+    galleryContainer.appendChild(gallery);
+    return galleryContainer;
   }
 }
 
