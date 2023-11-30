bd4e8f56af682772eb0d7f4029d393ab7179002a
Wonnnyo Hamester <wonnyohamester@outlook.com>
2023-11-29 19:19:49 -0800

    fix: logout redirect

--- pages/logout/script.js
+++ pages/logout/script.js
@@ -1,7 +1,9 @@
 import { clearUserSession } from '../../assets/helpers/storage.js';
 import { clearPrevCenter } from '../../assets/helpers/prev-center.js';
 
-clearUserSession();
-clearPrevCenter();
+window.onload = () => {
+  clearUserSession();
+  clearPrevCenter();
 
-window.location.replace('/pages/home');
+  window.location.assign('/pages/home/index.html');
+};
