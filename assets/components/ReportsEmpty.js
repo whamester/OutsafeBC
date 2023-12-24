a94864f3435208cdadf5a9728d325c9bce4a99ac
Reinhardt Botha <botha.wr@gmail.com>
2023-12-23 19:59:27 -0800

    updated margin

--- assets/components/ReportsEmpty.js
+++ assets/components/ReportsEmpty.js
@@ -6,7 +6,7 @@ class ReportsEmpty {
         <div style="display:flex; flex-direction:column; gap:2rem; justify-content:center; align-items:center; padding-top:3rem; padding-bottom:3rem; width: 100%;">
                 <img src="../../assets/img/notifications-empty.svg" style="width:19.13rem; height: 12.15rem"/>
                 <div style="display:flex; flex-direction:column; gap:0.75rem; width: 24.625rem">
-                        <h2 class="text-center text-heading-2">Looks like you haven't reported any hazards yet.</h2>
+                        <h2 class="text-center text-heading-2" style="margin: 0;">Looks like you haven't reported any hazards yet.</h2>
                         <p class="text-body-2 text-center" style="color:var(--neutral-600)">Start by reporting a hazard to ensure a safe outdoors experience for everyone!<p>
                 </div>
         </div>
